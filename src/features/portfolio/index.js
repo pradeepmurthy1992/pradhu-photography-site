696a5162c6a4f47966790814cab0307ea4bbdb9d
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-09-05 15:59:47 +0530

    Create index.js

---
+++ src/features/portfolio/index.js
@@ -0,0 +1,5 @@
+export { default } from "./Portfolio";
+export { default as Portfolio } from "./Portfolio";
+export { default as PortfolioLanding } from "./PortfolioLanding";
+export { default as PortfolioPage } from "./PortfolioPage";
+export { GH_CATEGORIES, GH_CATEGORIES_EXT } from "./categoriesMeta";
