7d619fbb86744c6fd2267f137445e312f239b1e0
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-09-05 16:30:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
+const isCI = process.env.CI === "true";
+// CHANGE THIS if deploying to a repo page: e.g. const base = "/pradhu-site/";
+const base = "/";
+
 export default defineConfig({
+  base,
   plugins: [react()],
   resolve: {
     alias: {
