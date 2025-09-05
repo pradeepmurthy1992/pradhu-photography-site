f9c5d1e49a4b566eb3cfd6c2efcf50c735f1b0a3
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-09-05 16:19:03 +0530

    Create routes.js

---
+++ src/app/routes.js
@@ -0,0 +1,3 @@
+// Minimal route list for the hash router (#/route)
+export const ROUTES = ["/", "/portfolio", "/services", "/pricing", "/about", "/reviews", "/contact", "/404"];
+export default ROUTES;
