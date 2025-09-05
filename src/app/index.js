8002fae7b8fc44f56278480a4f247171812ee525
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-09-05 16:21:26 +0530

    Create index.js

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+export { default as App } from "./App";
+export { default as useThemeTokens } from "./themeTokens";
+export * from "./themeTokens";
+export * from "./routes";
+export * from "./config";
+export * from "./track";
